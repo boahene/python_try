e5beb21c83d255b4d7ce924e1ee834ba43242784
Julius Osei-wusu Boahene <oseiwusuboahene@gmail.com>
2023-02-25 16:37:09 +0000

    my comic game

---
+++ comic.py
@@ -0,0 +1,24 @@
+print("***LET's HAVE SOME FUN ON COMIC***")
+print("🚫 rules of the game - YES/NO (BLOCK LETTERS ONLY) ")
+print()
+spiderman = input("Do you like sitting on top of buildings and taking a walk along the hallways?   ")
+if spiderman == "YES":
+  print("You are SPIDERMAN")
+else:
+  print("You cant be spider man")
+
+print()
+antman = input("Do you sometimes wish you can shrink into things, disappear and appear at will?   ")
+if antman == "YES":
+  print("You are antMan")
+else:
+  print("You cant be AntMan")
+
+print()
+moongirl = input("are you Young Genius and Inhuman, displays bravery beyond your age, uses vast intelligence and telepathic to overcome your demons?   ")
+if moongirl == "YES":
+  print("Hurray Kid you are friends with moon girl")
+else:
+  print("You could have partied with moon girl now")
+
+print("THANKS FOR YOUR TIME")
