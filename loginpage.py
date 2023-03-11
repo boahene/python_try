a8b8b491ac845fb64929cc7a1befe032edf05ce1
Julius Osei-wusu Boahene <oseiwusuboahene@gmail.com>
2023-03-11 12:26:21 +0000

    a sample login page with python

---
+++ loginpage.py
@@ -0,0 +1,13 @@
+
+  #sample login page wit py
+  print("SECURE LOGIN")
+print("--")
+username = input("Username > ")
+password = input("password > ")
+
+if username == "young" and password == "young":
+  print("welcome Young")
+elif username == "boahene" and password == "boahene":
+  print("hey there Boahene")
+else:
+  print("Go away")
