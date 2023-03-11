ece59f6f690d853f9123379618654e58d7fa9bcd
Julius Osei-wusu Boahene <oseiwusuboahene@gmail.com>
2023-03-11 12:03:07 +0000

    create if and elif statement

---
+++ elif.py
@@ -0,0 +1,10 @@
+print("SECURE LOGIN")
+print("--")
+username = input("Username > ")
+
+if username == "young":
+  print("welcome Young")
+elif username == "boahene":
+  print("hey there Boahene")
+else:
+  print("Go away")
