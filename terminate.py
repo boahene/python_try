1cbe9d56f13dc5089684c25b568123a0d4f5dd52
Julius Osei-wusu Boahene <oseiwusuboahene@gmail.com>
2023-04-20 23:43:49 +0000

    updated

--- terminate.py
+++ terminate.py
@@ -1,7 +1,7 @@
-while true:
+while True:
   print("the programm istill running")
   
   playAgain = input("Do you wanna still play this game> ")
   if playAgain = "no":
     break;
- print("Game terminated !!")
+print("Game terminated !!")
